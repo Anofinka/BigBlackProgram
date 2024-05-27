59a9546a15fe9bee27d2d56efc2b5deae843b14a
Niger <119509210+Anofinka@users.noreply.github.com>
2024-05-27 17:42:36 +0200

    items_prefab_tooltipFix
    items_prefab_tooltipFix

--- Assets/Scripts/TipMenager.cs
+++ Assets/Scripts/TipMenager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using System;
 using UnityEngine;
-using System.Runtime.CompilerServices;
 
 public class TipMenager : MonoBehaviour
 {
@@ -31,8 +30,39 @@ public class TipMenager : MonoBehaviour
         tipText.text = tip;
         tipWindow.sizeDelta = new Vector2(tipText.preferredHeight > 200 ? 200 : tipText.preferredWidth, tipText.preferredHeight);
         tipWindow.gameObject.SetActive(true);
-        tipWindow.transform.position = new Vector2(mousePos.x + tipWindow.sizeDelta.x + 50, mousePos.y);
 
+        // Calculate desired position
+        Vector2 newPosition = new Vector2(mousePos.x + tipWindow.sizeDelta.x + 50, mousePos.y);
+
+        // Get screen bounds
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        // Ensure the tooltip does not go off the right edge
+        if (newPosition.x + tipWindow.sizeDelta.x > screenWidth)
+        {
+            newPosition.x = screenWidth - tipWindow.sizeDelta.x;
+        }
+
+        // Ensure the tooltip does not go off the left edge
+        if (newPosition.x < 0)
+        {
+            newPosition.x = 0;
+        }
+
+        // Ensure the tooltip does not go off the top edge
+        if (newPosition.y + tipWindow.sizeDelta.y > screenHeight)
+        {
+            newPosition.y = screenHeight - tipWindow.sizeDelta.y;
+        }
+
+        // Ensure the tooltip does not go off the bottom edge
+        if (newPosition.y < 0)
+        {
+            newPosition.y = 0;
+        }
+
+        tipWindow.transform.position = newPosition;
     }
     private void HideTip()
     {
@@ -41,5 +71,5 @@ public class TipMenager : MonoBehaviour
     }
 
     // Update is called once per frame
-    
+
 }
