63931b4d76d308aeb05c3125f2293f45dc24dfb5
Niger <119509210+Anofinka@users.noreply.github.com>
2024-05-30 20:18:45 +0200

    .
    .

---
+++ Assets/Scripts/Open_stats_panel.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Stats_opener : MonoBehaviour
+{
+    public GameObject statsPanel; // Referencja do interfejsu u¿ytkownika ekwipunku
+
+    private void Awake()
+    {
+        nazero2();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) // SprawdŸ, czy klawisz "R" zosta³ naciœniêty
+        {
+            ToggleInventory2(); // Wywo³aj funkcjê do otwierania/ zamykania ekwipunku
+        }
+    }
+
+    void ToggleInventory2()
+    {
+        if (statsPanel.activeSelf)
+        {
+            nazero2();
+        }
+        else
+        {
+            statsPanel.SetActive(true);
+        }
+    }
+
+    void nazero2()
+    {
+        statsPanel.SetActive(false);
+    }
+}
